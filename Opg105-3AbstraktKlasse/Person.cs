9e229951a0c94b131dec83fa7f046b26e66da308
admin <admin@TA-829-05>
2020-10-06 15:53:43 +0200

    Tirsdag eftermiddag

---
+++ Opg105-3AbstraktKlasse/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opg105_3AbstraktKlasse
+{
+    abstract class Person
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+
+        public void Skriv1()
+        {
+            Console.WriteLine("Person med id "+Id+" og navn "+Navn);
+        }
+
+        public abstract void Skriv2();
+    }
+
+    class Kursist : Person
+    {
+        public override void Skriv2()
+        {
+            Console.WriteLine("Kursist med id " + Id + " og navn " + Navn);
+        }
+    }
+}
+
---
+++ Opg105-3AbstraktKlasse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Opg105_3AbstraktKlasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kursist k = new Kursist() { Id = 1, Navn = "a" };
+            k.Skriv1();
+            k.Skriv2();
+
+        }
+    }
+}
